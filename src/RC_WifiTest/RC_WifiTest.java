079bed76a380316c6a1f50cfc325ab9fdafa4677
kyChu <hello.kychu@gmail.com>
2018-01-02 18:36:46 +0800

    new project to display rc channel value

---
+++ src/RC_WifiTest/RC_WifiTest.java
@@ -0,0 +1,58 @@
+package RC_WifiTest;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+
+import MainFrame.MyMainFrame;
+import protocol.ComPackage;
+import protocol.RxAnalyse;
+
+public class RC_WifiTest extends MyMainFrame {
+	private static final long serialVersionUID = 1L;
+
+	private static ComPackage rxData = new ComPackage();
+
+	private JPanel MainPanel = null;
+	private JLabel ChannelLab_1 = new JLabel("000");
+	private JLabel ChannelLab_2 = new JLabel("000");
+	private JLabel ChannelLab_3 = new JLabel("000");
+	private JLabel ChannelLab_4 = new JLabel("000");
+
+	public RC_WifiTest() {
+		MyMainFrame frame = new MyMainFrame();
+		MainPanel = frame.getUsrMainPanel();
+		MainPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		MainPanel.add(ChannelLab_1); MainPanel.add(ChannelLab_2); MainPanel.add(ChannelLab_3); MainPanel.add(ChannelLab_4);
+		frame.setVisible(true);
+		frame.setAutoTxEnable(true);
+	}
+
+	public void RxDataProcess() {
+		synchronized(new String("")) {
+			try {
+				rxData = (ComPackage) RxAnalyse.RecPackage.PackageCopy();
+			} catch (CloneNotSupportedException e) {
+				e.printStackTrace();
+			}
+		}
+		if(rxData.type == ComPackage.TYPE_WIFI_RC_RAW) {
+			ChannelLab_1.setText(String.format("%.03d", rxData.readoutCharacter(0)));
+			ChannelLab_2.setText(String.format("%.03d", rxData.readoutCharacter(2)));
+			ChannelLab_3.setText(String.format("%.03d", rxData.readoutCharacter(4)));
+			ChannelLab_4.setText(String.format("%.03d", rxData.readoutCharacter(8)));
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+            UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println("Couldn't use system look and feel.");
+        }
+		new RC_WifiTest();
+	}
+}
