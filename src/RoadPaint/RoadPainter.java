7596299135bee3d7da15eaa1f564eefbe20e67d1
kyChu <hello.kychu@gmail.com>
2018-10-30 18:34:21 +0800

    update draw method

--- src/RoadPaint/RoadPainter.java
+++ src/RoadPaint/RoadPainter.java
@@ -1,10 +1,16 @@
 package RoadPaint;
 
+//import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
+//import java.awt.event.MouseAdapter;
+//import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JPanel;
 import javax.swing.UIManager;
@@ -19,38 +25,56 @@ public class RoadPainter extends MyMainFrame {
 	private static final long serialVersionUID = 1L;
 
 	private JPanel MainPanel = null;
+	private Image img = null;
+	private Graphics gPointer = null;
 	private myPanel Drawer = new myPanel();
+
+	private Point2D.Double[] pCross = new Point2D.Double[2];
 
 	public RoadPainter() {
 		this.setFrameSize(1000, 600);
 		MainPanel = this.getUsrMainPanel();
 		MainPanel.setLayout(new BorderLayout());
 		MainPanel.add(Drawer, BorderLayout.CENTER);
+		img = new BufferedImage(1000, 600, BufferedImage.TYPE_INT_RGB);
+		Drawer.setImage(img);
+		gPointer = img.getGraphics();
 		this.setResizable(true);
 		this.setVisible(true);
-	}
 
-	public static void main(String[] args) {
-		try {
-            UIManager.setLookAndFeel(
-                UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            System.err.println("Couldn't use system look and feel.");
-        }
-		new RoadPainter();
-	}
-}
-
-class myPanel extends JPanel {
-	private static final long serialVersionUID = 1L;
-	
-	private Graphics g;
-	private Point2D.Double[] pCross = new Point2D.Double[2];
-	public myPanel() {
 		pCross[0] = new Point2D.Double(0, 0);
 		pCross[1] = new Point2D.Double(0, 0);
+
+		new Thread(new TestThread()).start();
 	}
-	public void paint(Graphics g) {
+	
+	private class TestThread implements Runnable {
+		private Point pTest = new Point(0, 60);
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			MainPaint(gPointer);
+			while(true) {
+				pTest.x += 60;
+				if(pTest.x >= 600) {
+					pTest.x = 60;
+					pTest.y += 60;
+					if(pTest.y >= 600) {
+						pTest.y = 60;
+					}
+				}
+				DrawSignPoint(img.getGraphics(), pTest);
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public void MainPaint(Graphics g) {
 		g.setColor(Color.BLUE);
 		Point OrgPoint = new Point(this.getWidth() / 2, this.getHeight() / 2);
 		DrawSignPoint(g, OrgPoint);
@@ -80,11 +104,30 @@ class myPanel extends JPanel {
 		g.drawArc((int)(c.getX() - c.getRadius()), (int)(c.getY() - c.getRadius()), (int)c.getRadius() * 2, (int)c.getRadius() * 2, 0, 360);
 		g.setColor(org_color);
 	}
-	
-	public void display() {
-		if(g != null) {
-			super.paint(g);
-			this.repaint();
+
+	public static void main(String[] args) {
+		try {
+            UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println("Couldn't use system look and feel.");
+        }
+		new RoadPainter();
+	}
+}
+
+class myPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	private Image img;
+	public myPanel() {}
+	public void setImage(Image img) {
+		if(img != null) {
+			this.img = img;
 		}
+	}
+
+	public void paint(Graphics g) {
+		g.drawImage(img, 0, 0, this);
 	}
 }
