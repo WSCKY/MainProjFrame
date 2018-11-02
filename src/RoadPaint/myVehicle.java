497ccda6d1fa13dec15ba5697d734bd9a6e6d0cd
kyChu <hello.kychu@gmail.com>
2018-11-02 11:33:16 +0800

    update class

--- src/RoadPaint/myVehicle.java
+++ src/RoadPaint/myVehicle.java
@@ -20,9 +20,10 @@ public class myVehicle implements ImageObserver {
 
 	private static int ID = 0;
 
-	private BufferedImage imgORG, imgSCL, imgDST;
+	private BufferedImage imgORG, imgDST;
+	private BufferedImage imgZoom, imgName;
 	private Graphics gCanvas = null;
-	private String TagName = "Tag";
+	private String TagName = "TAG";
 	private static int id = 0;
 	private int xPos = 0, yPos = 0;
 	private int Yaw = 0;
@@ -34,9 +35,9 @@ public class myVehicle implements ImageObserver {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		zoom(defaultScale);
-		ShowName();
-		rotate(Yaw);
+		imgZoom = zoom(imgORG, defaultScale);
+		imgName = DrawName(imgZoom);
+		imgDST = rotate(imgName, Yaw);
 		id = ID ++;
 	}
 	public myVehicle(Graphics g) {
@@ -47,24 +48,25 @@ public class myVehicle implements ImageObserver {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		zoom(defaultScale);
-		ShowName();
-		rotate(Yaw);
+		imgZoom = zoom(imgORG, defaultScale);
+		imgName = DrawName(imgZoom);
+		imgDST = rotate(imgName, Yaw);
+		id = ID ++;
 	}
 
-    private void rotate(int deg) {
+    private BufferedImage rotate(BufferedImage img, int deg) {
         deg = deg % 360;
         if (deg < 0) deg = 360 + deg;
         double rad = Math.toRadians(deg);
         double sin_rad = Math.sin(rad);
         double cos_rad = Math.cos(rad);
-        int rw = (int) (Math.abs(imgSCL.getWidth() * cos_rad) + Math.abs(imgSCL.getHeight() * sin_rad));
-        int rh = (int) (Math.abs(imgSCL.getWidth() * sin_rad) + Math.abs(imgSCL.getHeight() * cos_rad));
+        int rw = (int) (Math.abs(img.getWidth() * cos_rad) + Math.abs(img.getHeight() * sin_rad));
+        int rh = (int) (Math.abs(img.getWidth() * sin_rad) + Math.abs(img.getHeight() * cos_rad));
 
-        int x = (rw / 2) - (imgSCL.getWidth() / 2);
-        int y = (rh / 2) - (imgSCL.getHeight() / 2);
+        int x = (rw / 2) - (img.getWidth() / 2);
+        int y = (rh / 2) - (img.getHeight() / 2);
 
-        imgDST = new BufferedImage(rw, rh, imgSCL.getType());
+        imgDST = new BufferedImage(rw, rh, img.getType());
         Graphics2D gs = (Graphics2D) imgDST.getGraphics();
         gs.setColor(new Color(0, 0, 0, 0));
         gs.fillRect(0, 0, rw, rh);
@@ -72,24 +74,26 @@ public class myVehicle implements ImageObserver {
         AffineTransform at = new AffineTransform();
         at.rotate(rad, rw / 2, rh / 2);
         at.translate(x, y);
-        new AffineTransformOp(at, AffineTransformOp.TYPE_BICUBIC).filter(imgSCL, imgDST);
+        return new AffineTransformOp(at, AffineTransformOp.TYPE_BICUBIC).filter(img, imgDST);
     }
-    private void zoom(double scale) {
-    	imgSCL = new BufferedImage((int)(imgORG.getWidth() * scale), (int)(imgORG.getHeight() * scale), imgORG.getType());
-		new AffineTransformOp(AffineTransform.getScaleInstance(scale, scale), null).filter(imgORG, imgSCL);
+    private BufferedImage zoom(BufferedImage img, double scale) {
+    	BufferedImage imgSCL = new BufferedImage((int)(img.getWidth() * scale), (int)(img.getHeight() * scale), img.getType());
+		return new AffineTransformOp(AffineTransform.getScaleInstance(scale, scale), null).filter(img, imgSCL);
     }
-    private void ShowName() {
-    	String s = TagName + "_" + id;
-    	Font f = new Font("Courier New", Font.BOLD, 18);
-    	int cw = imgSCL.getGraphics().getFontMetrics(f).stringWidth(s);
-    	int ch = imgSCL.getGraphics().getFontMetrics(f).getHeight();
-    	int w = Math.max(cw, imgSCL.getWidth());
-    	int h = Math.max(ch, imgSCL.getHeight());
-    	BufferedImage bi = new BufferedImage(w, h, imgSCL.getType());
-    	bi.getGraphics().setFont(f);
-    	bi.getGraphics().drawImage(imgSCL, (w - imgSCL.getWidth()) / 2, (h - imgSCL.getHeight()) / 2, this);
-    	bi.getGraphics().drawString(s, (w - cw) / 2, (h + ch) / 2);
-    	imgSCL = bi;
+    private BufferedImage DrawName(BufferedImage img) {
+    	String s = TagName + "(" + id + ")";
+    	Font f = new Font("Courier New", Font.BOLD, 16);
+    	Graphics g = img.getGraphics();
+    	int cw = g.getFontMetrics(f).stringWidth(s);
+    	int ch = g.getFontMetrics(f).getHeight();
+    	int ascent = g.getFontMetrics(f).getAscent();
+    	int w = Math.max(cw, img.getWidth());
+    	int h = ch + img.getHeight();
+    	BufferedImage imgName = new BufferedImage(w, h, img.getType());
+    	g = imgName.getGraphics(); g.setFont(f);
+    	g.drawImage(img, (w - img.getWidth()) / 2, 0, this);
+    	g.drawString(s, (w - cw) / 2, h - ch + ascent);
+    	return imgName;
     }
 
 	public Image getImage() {
@@ -109,21 +113,21 @@ public class myVehicle implements ImageObserver {
 	}
 	public void setYaw(int yaw) {
 		this.Yaw = yaw;
-		rotate(Yaw);
+		rotate(imgName, Yaw);
 	}
 	public void setZoom(double z) {
 		if(z > 16) z = 16;
 		if(z < 0.5) z = 0.5; 
-		zoom(defaultScale * z);
-		ShowName();
-		rotate(Yaw);
+		imgZoom = zoom(imgORG, defaultScale * z);
+		imgName = DrawName(imgZoom);
+		rotate(imgName, Yaw);
 	}
 	public void setZoomTo(int width) {
 		if(width < 9) width = 9;
 		if(width > imgSrcWidth) width = imgSrcWidth;
-		zoom((double)width / imgSrcWidth);
-		ShowName();
-		rotate(Yaw);
+		imgZoom = zoom(imgORG, (double)width / imgSrcWidth);
+		imgName = DrawName(imgZoom);
+		rotate(imgName, Yaw);
 	}
 	public void update() {
 		if(gCanvas != null) {
