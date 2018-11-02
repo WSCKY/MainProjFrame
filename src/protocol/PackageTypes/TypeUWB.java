9d488062f874fcac88bc0488ebca165cd3a25345
kyChu <hello.kychu@gmail.com>
2018-11-02 18:26:24 +0800

    add new type

--- src/protocol/PackageTypes/TypeUWB.java
+++ src/protocol/PackageTypes/TypeUWB.java
@@ -7,4 +7,6 @@ public class TypeUWB {
 	/* -------- version -------- */
 	public static final byte TYPE_VERSION_REQUEST = (byte)0x02;
 	public static final byte TYPE_VERSION_Response = (byte)0x03;
+	
+	public static final byte TYPE_DIST_Response = (byte)0x11;
 }
