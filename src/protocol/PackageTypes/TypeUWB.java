a596a222da8ba33610200bb9f4bca0112c286a64
kyChu <hello.kychu@gmail.com>
2018-11-02 17:31:08 +0800

    add new type

---
+++ src/protocol/PackageTypes/TypeUWB.java
@@ -0,0 +1,10 @@
+package protocol.PackageTypes;
+
+public class TypeUWB {
+	/* ########## package type ########## */
+	/* -------- Heart-beat -------- */
+	public static final byte TYPE_COM_HEARTBEAT = (byte)0x01;
+	/* -------- version -------- */
+	public static final byte TYPE_VERSION_REQUEST = (byte)0x02;
+	public static final byte TYPE_VERSION_Response = (byte)0x03;
+}
