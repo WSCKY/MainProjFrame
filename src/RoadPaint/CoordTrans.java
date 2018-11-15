0cb1b9ef80b8e08e6955fb93c622c956ef581b6d
kyChu <hello.kychu@gmail.com>
2018-11-15 22:58:14 +0800

    add UI to set coordinate transform

--- src/RoadPaint/CoordTrans.java
+++ src/RoadPaint/CoordTrans.java
@@ -1,13 +1,27 @@
 package RoadPaint;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.Point;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.Point2D;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.UIManager;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class CoordTrans extends JPanel {
 	private static final long serialVersionUID = 1L;
@@ -15,6 +29,8 @@ public class CoordTrans extends JPanel {
 	private JCheckBox swapCB = new JCheckBox("SWAP");
 	private JCheckBox xMirCB = new JCheckBox("MirrorX");
 	private JCheckBox yMirCB = new JCheckBox("MirrorY");
+	private JSlider scaleSlider = new JSlider(-100, 100);
+	Dictionary<Integer, Component> labelTable = new Hashtable<Integer, Component>();
 
 	private int UI_Width = 800, UI_Height = 800; // default 800x800 (pixels)
 	private double Real_xSize = 20.0, Real_ySize = 20.0; // default 20.0x20.0 (unit:m)
@@ -28,12 +44,26 @@ public class CoordTrans extends JPanel {
 	private boolean Y_Mirror = false;
 
 	private void initUI() {
+		this.setLayout(new BorderLayout());
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(3, 1));
+		p.add(swapCB); p.add(xMirCB); p.add(yMirCB);
 		swapCB.setFont(new Font("Courier New", Font.BOLD, 16));
 		xMirCB.setFont(new Font("Courier New", Font.BOLD, 16));
 		yMirCB.setFont(new Font("Courier New", Font.BOLD, 16));
-		this.add(swapCB);
-		this.add(xMirCB);
-		this.add(yMirCB);
+		swapCB.addItemListener(ChangeIL);
+		xMirCB.addItemListener(ChangeIL);
+		yMirCB.addItemListener(ChangeIL);
+		this.add(p, BorderLayout.CENTER);
+		scaleSlider.setPaintLabels(true);
+		labelTable.put(-100, new JLabel("-")); labelTable.put(0, new JLabel("0")); labelTable.put(100, new JLabel("+"));
+		scaleSlider.setLabelTable(labelTable);
+		scaleSlider.setPreferredSize(new Dimension(250, 40));
+		scaleSlider.addChangeListener(SliderCL);
+		scaleSlider.addMouseListener(SliderML);
+		JPanel ps = new JPanel();
+		ps.add(scaleSlider);
+		this.add(ps, BorderLayout.SOUTH);
 	}
 
 	public CoordTrans() { initUI(); updateGain(); }
@@ -78,6 +108,62 @@ public class CoordTrans extends JPanel {
 
 		TransGain = (s1 < s2) ? s1 : s2;
 	}
+
+	ChangeListener SliderCL = new ChangeListener() {
+		@Override
+		public void stateChanged(ChangeEvent e) {
+			// TODO Auto-generated method stub
+			int v = ((JSlider)e.getSource()).getValue();
+			if(v > 0) UserScale = 1.0 + ((double)v / 100);
+			if(v < 0) UserScale = 1.0 + ((double)v / 200);
+		}
+	};
+	ItemListener ChangeIL = new ItemListener() {
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			// TODO Auto-generated method stub
+			JCheckBox cb = (JCheckBox)e.getSource();
+			if(cb == swapCB) {
+				XY_SWAP = swapCB.isSelected();
+//				System.out.println("SWAP:"+XY_SWAP);
+			} else if(cb == xMirCB) {
+				X_Mirror = xMirCB.isSelected();
+//				System.out.println("X:"+X_Mirror);
+			} else if(cb == yMirCB) {
+				Y_Mirror = yMirCB.isSelected();
+//				System.out.println("Y:"+Y_Mirror);
+			}
+		}
+	};
+	MouseListener SliderML = new MouseListener() {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			TransGain = TransGain * UserScale;
+			UserScale = 1.0;
+			scaleSlider.setValue(0);
+		}
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
 
 	public int Real2UI(double l) { return (int) (l * TransGain * UserScale); }
 	public Point Real2UI(double x, double y) {
