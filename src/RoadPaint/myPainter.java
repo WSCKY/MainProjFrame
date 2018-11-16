eaf99ae33add1d8d9ee50fcaf03f67208e030ffe
kyChu <hello.kychu@gmail.com>
2018-11-16 18:31:46 +0800

    update painter

--- src/RoadPaint/myPainter.java
+++ src/RoadPaint/myPainter.java
@@ -1,13 +1,22 @@
 package RoadPaint;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 
 public class myPainter {
 	private Graphics gCanvas = null;
+
+	private static final int MAX_POINT = 50;
+	private static int pointIndexCnt = 0;
+	private static Point[] pointList = new Point[MAX_POINT];
 	public myPainter(Graphics g) {
 		gCanvas = g;
+		for(int i = 0; i < MAX_POINT; i ++) {
+			pointList[i] = new Point(0, 0);
+		}
 	}
 	public void drawAnchorSign(Point p) {
 		gCanvas.setColor(Color.BLUE);
@@ -28,5 +37,36 @@ public class myPainter {
 	public void drawCircleShip(Point p, int r) {
 		gCanvas.setColor(Color.RED);
 		gCanvas.drawArc(p.x-r, p.y-r, r*2, r*2, 0, 360);
+	}
+	public void drawBoldLine(Point a, Point b, int w, Color c) {
+		Graphics2D g2d = (Graphics2D)gCanvas;
+		g2d.setStroke(new BasicStroke(w));
+		g2d.drawLine(a.x, a.y, b.x, b.y);
+	}
+
+	public void addLocusPoint(Point p) {
+		pointList[pointIndexCnt] = p;
+		pointIndexCnt ++;
+		if(pointIndexCnt >= MAX_POINT)
+			pointIndexCnt = 0;
+	}
+	public void drawLocus() {
+		int i = pointIndexCnt, j = 0;
+		int colorStep = 255 / MAX_POINT;
+		int alpha = 255;
+		Point p1, p2 = new Point(0, 0);
+		Graphics2D g2d = (Graphics2D)gCanvas;
+		g2d.setStroke(new BasicStroke(5));
+		do {
+			alpha = j * colorStep;
+			g2d.setColor(new Color(255, 0, 0, alpha));
+			p1 = pointList[i ++];
+			if(i >= MAX_POINT) i = 0;
+			if(j > 0) {
+				g2d.drawLine(p2.x, p2.y, p1.x, p1.y);
+			}
+			p2 = p1;
+			j ++;
+		} while(j < MAX_POINT);
 	}
 }
