22d6939f4d7f0c4e50c03e7fd3352d088f2be762
kyChu <hello.kychu@gmail.com>
2018-11-21 23:16:01 +0800

    use XYSeries chart

--- src/WaveTool/WaveTool.java
+++ src/WaveTool/WaveTool.java
@@ -3,56 +3,60 @@ package WaveTool;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.time.Millisecond;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.general.SeriesChangeEvent;
+import org.jfree.data.general.SeriesChangeListener;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
-public class WaveTool extends ChartPanel {
+public class WaveTool extends ChartPanel implements SeriesChangeListener {
 	private static final long serialVersionUID = 1L;
 
 	private static JFreeChart freeChart = null;
-	private static TimeSeriesCollection DataSet = null;
-	private static ArrayList<TimeSeries> TimeSeriesList = new ArrayList<TimeSeries>();
+	private static XYSeriesCollection DataSet = null;
+	private static ArrayList<mySeries> SeriesList = new ArrayList<mySeries>();
+
 	public WaveTool(String Title) {
 		super(createChart(Title));
 	}
 
 	private static JFreeChart createChart(String title) {
-		DataSet = new TimeSeriesCollection();
-		freeChart = ChartFactory.createTimeSeriesChart(title, "Time", "", DataSet, true, true, false);
+		DataSet = new XYSeriesCollection();
+		freeChart = ChartFactory.createXYLineChart(title, "Time", "", DataSet, PlotOrientation.VERTICAL, true, true, false);
 		XYPlot xyplot = freeChart.getXYPlot();
 		ValueAxis valueaxis = xyplot.getDomainAxis();
 		valueaxis.setAutoRange(true);
 //		valueaxis.setInverted(true);
-		valueaxis.setFixedAutoRange(5000);
+		valueaxis.setFixedAutoRange(500);
 		valueaxis.setTickLabelFont(new Font("Courier New", Font.BOLD, 12));
 		valueaxis.setLabelFont(new Font("Courier New", Font.BOLD, 14));
 		xyplot.getRenderer().setSeriesStroke(1, new BasicStroke(3.0f));
-		DateAxis dateaxis = ((DateAxis)valueaxis);
-		dateaxis.setDateFormatOverride(new SimpleDateFormat("HH-mm-ss"));
 		valueaxis = xyplot.getRangeAxis();
+		valueaxis.setAutoRange(true);
+		NumberAxis numAxis = ((NumberAxis)valueaxis);
+		numAxis.setAutoRangeIncludesZero(false);
 		freeChart.getLegend().setVisible(false);
 		return freeChart;
 	}
 
 	public void addNewSeries(String SeriesName) {
-		TimeSeries series = new TimeSeries(SeriesName);
-		TimeSeriesList.add(series);
+		mySeries series = new mySeries(SeriesName);
+		series.addChangeListener(this);
+		SeriesList.add(series);
 		DataSet.addSeries(series);
 	}
 	public void removeSeries(int index) {
-		if(TimeSeriesList.size() > index) {
-			DataSet.removeSeries(TimeSeriesList.get(index));
-			TimeSeriesList.remove(index);
+		if(SeriesList.size() > index) {
+			DataSet.removeSeries(SeriesList.get(index));
+			SeriesList.remove(index);
 		}
 	}
 	public void setTitle(String title) {
@@ -63,20 +67,48 @@ public class WaveTool extends ChartPanel {
 	}
 	public void removeAllSeries() {
 		DataSet.removeAllSeries();
-		TimeSeriesList.clear();
+		SeriesList.clear();
 	}
-	public int getSeriesNumber() {
-		return TimeSeriesList.size();
+	public int getSeriesCount() {
+		return SeriesList.size();
 	}
-	public void addDataToSeries(int index, double val) {
-		if(TimeSeriesList.size() > index) {
-			TimeSeriesList.get(index).add(new Millisecond(), val);
+	public void addDataToSeries(int index, double y) {
+		if(SeriesList.size() > index) {
+			mySeries s = SeriesList.get(index);
+			s.add(s.indexer ++, y);
 		}
+	}
+	public void setAutoRange(boolean auto) {
+		freeChart.getXYPlot().getRangeAxis().setAutoRange(auto);
+	}
+	public void setLockZeroPoint(boolean flag) {
+		NumberAxis numAxis = ((NumberAxis)freeChart.getXYPlot().getRangeAxis());
+		numAxis.setAutoRangeIncludesZero(flag);
+	}
+	public void setDataPoints(int n) {
+		freeChart.getXYPlot().getDomainAxis().setFixedAutoRange(n);
+	}
+	public void setAutoRangeMinimumSize(double size) {
+		freeChart.getXYPlot().getRangeAxis().setAutoRangeMinimumSize(size);
 	}
 	public void setSeriesColor(int index, Color c) {
 		freeChart.getXYPlot().getRenderer().setSeriesPaint(index, c);
 	}
 	public void setSeriesStroke(int index, float width) {
 		freeChart.getXYPlot().getRenderer().setSeriesStroke(index, new BasicStroke(width));
+	}
+
+	private class mySeries extends XYSeries {
+		private static final long serialVersionUID = 1L;
+		int indexer = 0;
+		public mySeries(Comparable<String> key) {
+			super(key);
+			// TODO Auto-generated constructor stub
+		}
+	}
+
+	@Override
+	public void seriesChanged(SeriesChangeEvent arg0) {
+		// TODO Auto-generated method stub
 	}
 }
