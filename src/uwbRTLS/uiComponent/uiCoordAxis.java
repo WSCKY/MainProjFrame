f54feef76c48f2c0c435759ce5e7a33007a20d32
kyChu <hello.kychu@gmail.com>
2018-12-04 19:55:39 +0800

    update coordiate ui

--- src/uwbRTLS/uiComponent/uiCoordAxis.java
+++ src/uwbRTLS/uiComponent/uiCoordAxis.java
@@ -5,21 +5,68 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class uiCoordAxis extends uiComponent {
-	private static final int AxisLength = 150;
+	private static final int AxisLength = 160;
+	private static final int TailLength = 25;
+	private static final int ArrowSize = 20;
+	private static final double ArrowAngleTan = 0.3;
+
+	private Graphics2D g;
+	private boolean X_Mirror = false, Y_Mirror = false;
 	public uiCoordAxis(int xp, int yp) {
 		super(AxisLength, AxisLength);
-		Graphics2D g = (Graphics2D)this.getGraphics();
+		g = (Graphics2D)this.getGraphics();
 		g.setColor(Color.BLACK);
 		g.setStroke(new BasicStroke(2));
-		g.drawLine(0, 20, AxisLength, 20);
-		g.drawLine(20, 0, 20, AxisLength);
-		g.drawLine(AxisLength - 20, 10, AxisLength, 20);
-		g.drawLine(AxisLength - 20, 30, AxisLength, 20);
-		g.drawLine(10, AxisLength - 20, 20, AxisLength);
-		g.drawLine(29, AxisLength - 20, 20, AxisLength);
+		g.drawLine(0, TailLength, AxisLength, TailLength); // horizon
+		g.drawLine(TailLength, 0, TailLength, AxisLength); // vertical
+		g.drawLine(AxisLength - ArrowSize, (int)(TailLength - ArrowSize * ArrowAngleTan), AxisLength, TailLength); // h-axis
+		g.drawLine(AxisLength - ArrowSize, (int)(TailLength + ArrowSize * ArrowAngleTan), AxisLength, TailLength);
+		g.drawLine((int)(TailLength - ArrowSize * ArrowAngleTan), AxisLength - ArrowSize, TailLength, AxisLength); // v-axis
+		g.drawLine((int)(TailLength + ArrowSize * ArrowAngleTan), AxisLength - ArrowSize, TailLength, AxisLength);
 		this.setPos(xp, yp);
 	}
 	public void setPos(int x, int y) {
-		super.setPos(x - 20, y - 20);
+		super.setPos(x - TailLength, y - TailLength);
+	}
+	public void xMirror(boolean flag) {
+		
+	}
+	public void yMirror(boolean flag) {
+		
+	}
+	private void drawAxis() {
+		g.setColor(new Color(0, 0, 0, 0));
+		g.fillRect(0, 0, AxisLength, AxisLength);
+		g.setColor(Color.BLACK);
+		g.setStroke(new BasicStroke(2));
+		if(X_Mirror) {
+			g.drawLine(AxisLength - TailLength, 0, AxisLength - TailLength, AxisLength);
+		} else {
+			g.drawLine(TailLength, 0, TailLength, AxisLength);
+		}
+		if(Y_Mirror) {
+			g.drawLine(0, AxisLength - TailLength, AxisLength, AxisLength - TailLength);
+		} else {
+			g.drawLine(0, TailLength, AxisLength, TailLength);
+		}
+		if(X_Mirror) {
+			if(Y_Mirror) {
+//				g.drawLine(AxisLength - ArrowSize, (int)(TailLength - ArrowSize * ArrowAngleTan), AxisLength, TailLength); // x-axis
+//				g.drawLine(AxisLength - ArrowSize, (int)(TailLength + ArrowSize * ArrowAngleTan), AxisLength, TailLength);
+//				g.drawLine((int)(TailLength - ArrowSize * ArrowAngleTan), AxisLength - ArrowSize, TailLength, AxisLength); // y-axis
+//				g.drawLine((int)(TailLength + ArrowSize * ArrowAngleTan), AxisLength - ArrowSize, TailLength, AxisLength);
+			} else {
+				
+			}
+		} else {
+			if(Y_Mirror) {
+				
+			} else {
+				g.drawLine(AxisLength - ArrowSize, (int)(TailLength - ArrowSize * ArrowAngleTan), AxisLength, TailLength); // h-axis
+				g.drawLine(AxisLength - ArrowSize, (int)(TailLength + ArrowSize * ArrowAngleTan), AxisLength, TailLength);
+				g.drawLine((int)(TailLength - ArrowSize * ArrowAngleTan), AxisLength - ArrowSize, TailLength, AxisLength); // v-axis
+				g.drawLine((int)(TailLength + ArrowSize * ArrowAngleTan), AxisLength - ArrowSize, TailLength, AxisLength);
+			}
+		}
 	}
 }
